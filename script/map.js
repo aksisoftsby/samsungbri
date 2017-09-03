d6170254cda2167ec0429032020ce1a6b34cedc5
aksisoftsby <aksisoftsby@gmail.com>
2017-09-03 08:59:13 +0700

    oke mas

--- script/map.js
+++ script/map.js
@@ -1,3 +1,22 @@
+
+// where	φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km);
+// note that angles need to be in radians to pass to trig functions!
+
+/**
+ R = 6371e3; // metres
+ φ1 = lat1.toRadians();
+ φ2 = lat2.toRadians();
+ Δφ = (lat2 - lat1).toRadians();
+ Δλ = (lon2 - lon1).toRadians();
+ 
+ a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+ Math.cos(φ1) * Math.cos(φ2) *
+ Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+ c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+ d = R * c;
+ **/
+
+
 var BriMap = {
   map: {},
   watchId: {},
@@ -9,18 +28,56 @@ var BriMap = {
   createMarker: function createMarker(place) {
     placeLoc = place.geometry.location;
     marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
+      map: BriMap.map,
+      position: place.geometry.location,
+      icon: image = {
+        url: place.icon,
+        scaledSize: new google.maps.Size(20, 20), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+      }
     });
+    // console.log(place);
+    /**
+     dist = this.getDistance(
+     this.lattitude, place.geometry.location.lat(),
+     this.longitude, place.geometry.location.lng()
+     );
+     **/
+    dist = geolib.getDistance(
+      {latitude: this.lattitude, longitude: this.longitude},
+      {latitude: place.geometry.location.lat(), longitude: place.geometry.location.lng()}
+    );
+    // console.log(dist);
     google.maps.event.addListener(marker, 'click', function () {
-      BriMap.infowindow.setContent(place.name);
+      k = place.name + "<br />" +
+        place.formatted_address + "<br />" +
+        "Jarak: " + dist + " meter"
+        ;
+      BriMap.infowindow = new google.maps.InfoWindow();
+      BriMap.infowindow.setContent(k);
       BriMap.infowindow.open(map, this);
     });
   },
+  rad: function (x) {
+    return x * Math.PI / 180;
+  },
+  getDistance: function (lat1, lat2, long1, long2) {
+    R = 6371e3; // metres
+    φ1 = this.rad(lat1);
+    φ2 = this.rad(lat2);
+    λ1 = this.rad(long1);
+    λ2 = this.rad(long2);
+
+    x = Math.round((λ2 - λ1) * Math.cos((φ1 + φ2) / 2) * R);
+    y = Math.round((φ2 - φ1) * R);
+
+    return {est: x, nord: y};
+  },
   init: function () {
-    this.infowindow = new google.maps.InfoWindow();
     if (navigator.geolocation) {
-      watchId = navigator.geolocation.watchPosition(
+      // watchId = navigator.geolocation.watchPosition(
+      watchId = navigator.geolocation.getCurrentPosition(
         // success function
           function (position) {
             console.log(position);
@@ -30,9 +87,13 @@ var BriMap = {
             store.set(BriMap.long, BriMap.longitude);
             $("#map").html("Mencari Bank BRI terdekat ...");
             lokasinasabah = new google.maps.LatLng(BriMap.lattitude, BriMap.longitude);
-            map = new google.maps.Map(document.getElementById('map'), {
+            BriMap.map = new google.maps.Map(document.getElementById('map'), {
               center: lokasinasabah,
               zoom: 14
+            });
+            marker = new google.maps.Marker({
+              map: BriMap.map,
+              position: {lat: BriMap.lattitude, lng: BriMap.longitude},
             });
             var request = {
               location: lokasinasabah,
@@ -43,12 +104,12 @@ var BriMap = {
               language: "id",
               query: "'BANK BRI' or 'ATM BRI'"
             };
-            service = new google.maps.places.PlacesService(map);
+            service = new google.maps.places.PlacesService(BriMap.map);
             // service.nearbySearch(request,
             service.textSearch(request,
               function (results, status) {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
-                  console.log(results);
+                  // console.log(results);
                   for (var i = 0; i < results.length; i++) {
                     var place = results[i];
                     BriMap.createMarker(results[i]);
